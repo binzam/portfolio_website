40e9868d27db68ca6cebeb48f97b6445b2ef4a92
binzam <btechan@gmail.com>
2025-03-31 02:20:04 +0300

    update: update link to project

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -10,8 +10,8 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
   const { screenshots, tools, links, detail = '', title } = project;
   const [isExpanded, setIsExpanded] = useState(false);
   const previewText = useMemo(() => {
-    if (detail.length <= 160) return detail;
-    const trimmed = detail.substring(0, 160);
+    if (detail.length <= 150) return detail;
+    const trimmed = detail.substring(0, 150);
     return trimmed.substring(0, trimmed.lastIndexOf(' ')) + '...';
   }, [detail]);
 
@@ -38,7 +38,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
       <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"></path></svg>
         <h3>{title}</h3>
         <p>{isExpanded ? detail : previewText}</p>
-        {detail.length > 160 && (
+        {detail.length > 150 && (
           <button
             onClick={() => setIsExpanded(!isExpanded)}
             aria-expanded={isExpanded}
